2d366a898fdf270a6500a520ae8c2a1230f72e40
alanrychert <alanrychert@gmail.com>
2023-10-10 17:42:56 -0300

    complete and working (still needs refactor)

--- scape-gpt/Assets/Scripts/Shelf.cs
+++ scape-gpt/Assets/Scripts/Shelf.cs
@@ -6,8 +6,8 @@ using TMPro;
 public class Shelf : UnlockableInteractable
 {
     protected override void OpenAction(){
-        key.ToShelfScale();
         key.MoveToObject(this.gameObject);
+        key.ToShelfScale();
         key.SetVisibility(true);
     }
 }
